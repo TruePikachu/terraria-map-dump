3b84e844ed7256d59a6f699d36ad6e85751726a7
Chris Dusto <cricket_lover@live.com>
2017-01-29 11:45:07 -0800

    Added :BIOME-SPREAD map render type

--- color.lisp
+++ color.lisp
@@ -2,7 +2,7 @@
   (:nicknames :tmapdump.color)
   (:use :common-lisp)
   (:export :%rgba :color :color-a :color-b :color-g :color-p :color-r
-           :copy-color :invert-color))
+           :copy-color :grayscale-color :invert-color))
 (in-package :terraria-map-dump.color)
 
 (deftype color () '(vector (unsigned-byte 8) 4))
@@ -14,10 +14,16 @@
   (b 255 :type (unsigned-byte 8))
   (a 255 :type (unsigned-byte 8)))
 
+(defun grayscale-color (color)
+  "Return grayscale representation of COLOR"
+  (let* ((sigma (+ (color-r color) (color-g color) (color-b color)))
+         (mean (round sigma 3)))
+    (%rgba mean mean mean
+           (color-a color))))
+
 (defun invert-color (color)
   "Return the inverse of COLOR"
   (%rgba (- 255 (color-r color))
          (- 255 (color-g color))
          (- 255 (color-b color))
          (color-a color)))
-
--- render-png.lisp
+++ render-png.lisp
@@ -4,6 +4,19 @@
   (:export :render-png))
 (in-package :terraria-map-dump.render-png)
 
+(defun biome-spread-rgba (tile y h)
+  (let ((sets (when (or (typep tile 'block-tile)
+                        (typep tile 'wall-tile))
+                (tile-sets tile))))
+    (if (or (find :corrupt sets)
+            (find :crimson sets)
+            (find :hallow sets))
+      (tile-raw-rgba tile y h)
+      (let ((color (grayscale-color
+                     (tile-rgba tile y h))))
+        (%rgba (color-r color) (color-g color) (color-b color)
+               (round (color-a color) 2))))))
+
 (defun render-png (map file-or-stream &optional (map-type :default))
   "Render MAP to FILE-OR-STREAM"
   (etypecase file-or-stream
@@ -11,7 +24,7 @@
                                    :direction :output
                                    :if-exists :supersede
                                    :element-type '(unsigned-byte 8))
-                (render-png map out)))
+                (render-png map out map-type)))
     (stream
       (let* ((stream file-or-stream)
              (data (minimap-data map))
@@ -19,9 +32,9 @@
                                    :color-type :truecolor-alpha
                                    :width (array-dimension data 1)
                                    :height (array-dimension data 0)))
-             (color-fn
-               (ecase map-type
-                 (:default #'tile-rgba))))
+             (color-fn (ecase map-type
+                         (:default #'tile-rgba)
+                         (:biome-spread #'biome-spread-rgba))))
         (zpng:start-png image stream)
         (dotimes (y (array-dimension data 0))
           (dotimes (x (array-dimension data 1))
