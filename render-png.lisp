36e60c02916bf4f54ed86cb94d20e16f98b64a52
Chris Dusto <cricket_lover@live.com>
2017-01-29 11:17:46 -0800

    Added the framework for different types of map renders

--- render-png.lisp
+++ render-png.lisp
@@ -4,7 +4,8 @@
   (:export :render-png))
 (in-package :terraria-map-dump.render-png)
 
-(defun render-png (map file-or-stream)
+(defun render-png (map file-or-stream &optional (map-type :default))
+  "Render MAP to FILE-OR-STREAM"
   (etypecase file-or-stream
     (pathname (with-open-file (out file-or-stream
                                    :direction :output
@@ -17,11 +18,14 @@
              (image (make-instance 'pixel-streamed-png
                                    :color-type :truecolor-alpha
                                    :width (array-dimension data 1)
-                                   :height (array-dimension data 0))))
+                                   :height (array-dimension data 0)))
+             (color-fn
+               (ecase map-type
+                 (:default #'tile-rgba))))
         (zpng:start-png image stream)
         (dotimes (y (array-dimension data 0))
           (dotimes (x (array-dimension data 1))
-            (let ((color (tile-rgba (aref data y x) y (array-dimension data 0))))
+            (let ((color (funcall color-fn (aref data y x) y (array-dimension data 0))))
               (write-pixel (coerce color 'list) image))))
         (zpng:finish-png image))))
   file-or-stream)
